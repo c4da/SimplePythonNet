0090c37112b8ccf7fc9a707f8e3ed6d45fc654ad
Miloš Čadek <cadek@extech.eu>
2022-11-26 20:27:20 +0100

    update

--- src/Network.py
+++ src/Network.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 """
@@ -28,6 +30,31 @@ class Network(object):
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a) + b)
         return a
+
+    def SGD(self, trainingData : np.array, epochs : int, miniBatchSize : int, learningRate : float, testData : np.array = None):
+        if testData:
+            n_test = len(testData)
+            n = len(trainingData)
+            for j in range(epochs):
+                random.shuffle(trainingData)
+                mini_batches = [trainingData[k:k + miniBatchSize] for k in range(0, n, miniBatchSize)]
+                for miniBatch in mini_batches:
+                    self.updateMiniBatch(miniBatch, learningRate)
+
+                if testData:
+                    print("Epoch {0}: {1} / {2}".format(j, self.evaluate(testData), n_test))
+                else:
+                    print("Epoch {0} complete".format(j))
+
+    def updateMiniBatch(self, miniBatch : np.array, eta : float):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in miniBatch:
+            delta_nabla_b, delta_nabla_w = self.backProp(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            self.weights = [w - (eta / len(miniBatch)) * nw for w, nw in zip(self.weights, nabla_w)]
+
 
 
 def sigmoid(z: np.array) -> np.array:
